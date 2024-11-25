77564bb145039e61523b8ff462a07ca7c80948d5
Daniele Mansillo <danielemansillo@Danieles-MacBook-Pro.fritz.box>
2024-11-25 22:50:16 +0100

    Managing old transformers version for E5-V

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,11 @@
-from .DSE_Embedder import DSE_Embedder
-from .E5V_Embedder import E5V_Embedder
-from .InternVL_DescriptionGenerator import InternVL_DescriptionGenerator
-from .Qwen2_5_LLM import Qwen2_5_LLM
-from .Qwen2VL_DescriptionGenerator import Qwen2VL_DescriptionGenerator
-from .Qwen2VL_LLM import Qwen2VL_LLM
-from .Stella_Embedder import Stella_Embedder
+from importlib.metadata import version
+
+if version("transformers" == "4.41.2"):
+    from .E5V_Embedder import E5V_Embedder
+else:
+    from .DSE_Embedder import DSE_Embedder
+    from .InternVL_DescriptionGenerator import InternVL_DescriptionGenerator
+    from .Qwen2_5_LLM import Qwen2_5_LLM
+    from .Qwen2VL_DescriptionGenerator import Qwen2VL_DescriptionGenerator
+    from .Qwen2VL_LLM import Qwen2VL_LLM
+    from .Stella_Embedder import Stella_Embedder
